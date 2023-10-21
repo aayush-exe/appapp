1a07c460ef4a74fabf6860e7dfae975d7fb48822
aayush.sehgal16@gmail.com <aayush.sehgal16@gmail.com>
2023-10-21 19:33:43 +0000

    Created Form2.ItemTemplate2_copy; Edited Form2 and Form2.ItemTemplate2

--- client_code/Form2/ItemTemplate2_copy/__init__.py
+++ client_code/Form2/ItemTemplate2_copy/__init__.py
@@ -1,15 +1,13 @@
-from ._anvil_designer import Form2Template
+from ._anvil_designer import ItemTemplate2_copyTemplate
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.server
 
-class Form2(Form2Template):
+class ItemTemplate2_copy(ItemTemplate2_copyTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.panel1.items = app_tables.convos.search()
-    self.panel1_copy.items = app_tables.convos.search()
 
     # Any code you write here will run before the form opens.
